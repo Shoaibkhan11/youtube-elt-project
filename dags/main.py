889cc13f408afda37ee6e41b7920888d3e167f09
Shoaib Khan <suhelsuh11374@yahoo.com>
2026-06-05 19:00:21 +0530

    Added final changes in DAGs

--- dags/main.py
+++ dags/main.py
@@ -3,8 +3,6 @@ import pendulum
 from datetime import datetime,timedelta
 from api.video_stats import get_playlist_id,get_video_ids,extract_video_data,save_to_json
 
-from datawarehouse.dwh import staging_table,core_table
-
 #define local timezone
 local_tz=pendulum.timezone("Asia/Kolkata")
 
@@ -37,16 +35,3 @@ with DAG(
 
     # define dependencies
     playlist_id >> video_id >> extracted_data >> save_to_json_task
-
-with DAG(
-    dag_id='update_db',
-    default_args=default_args,
-    description="DAG to process JSON file and insert data into both staging and core schemas",
-    schedule='0 15 * * *',
-    catchup=False
-) as dag:
-    
-    update_staging=staging_table()
-    update_core=core_table()
-
-    update_staging >> update_core
